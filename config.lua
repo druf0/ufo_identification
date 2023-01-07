c1215dad9a97dba47f0b8bc4442186450b180296
druf0 <metonegta@gmail.com>
2023-01-06 23:37:56 -0300

    config changes

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 Config = {}
 
-Config.Locale = GetConvar('esx:locale', 'en')
+-- You can start modifying below this.
 
+Config.Locale = GetConvar('esx:locale', 'en') -- Currently available in english and spanish.
 
+Config.Distance = 5 -- Distance between players to show their documentation
 
-Config.Distance = 5
+Config.Notifications = true -- true if you want notifications with ox_lib or false if you don't want any kind of notification
 
-Config.Notifications = true
-
-Config.Nationality = 'American'
+Config.Nationality = 'American' -- You can change this to the whatever nationality you want.
