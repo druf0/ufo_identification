4826cbc772152fa1fffff988a5c5915e8e10b3d4
druf0 <metonegta@gmail.com>
2023-01-07 21:28:56 -0300

    check for updates

--- config.lua
+++ config.lua
@@ -2,7 +2,8 @@ Config = {}
 
 -- You can start modifying below this.
 
-Config.checkForUpdates = true -- Check for updates?
+Config.Update = true -- True if you wanna check for new updates, false if you don't.
+
 
 Config.Locale = GetConvar('esx:locale', 'en') -- Currently available in english and spanish.
 
@@ -10,4 +11,4 @@ Config.Distance = 5 -- Distance between players to show their documentation
 
 Config.Notifications = true -- true if you want notifications with ox_lib or false if you don't want any kind of notification
 
-Config.Nationality = 'American' -- You can change this to the whatever nationality you want.
+Config.Nationality = 'American' -- You can change this to whatever nationality you want.
